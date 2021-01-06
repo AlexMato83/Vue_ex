facb503a5965d236a257c1f37c5042cfbff08738
AlexMato83 <matontialessandro0@gmail.com>
2021-01-06 17:38:49 +0100

    inizio

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="La prima applicazione Vue di" msg2="Alessandro Matonti"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
